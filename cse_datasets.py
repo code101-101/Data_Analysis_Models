dd79066b4ba43063f010eb27dc921a8c91db2e34
code101-101 <s62612208@gmail.com>
2024-11-30 01:06:22 +0530

    cse_datasets.py

--- cse_datasets.py
+++ cse_datasets.py
@@ -31,7 +31,6 @@ print(regr.score(X_test, Y_test))
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import random
